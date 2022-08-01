490a087371e331b3bbe33e8615aa8d6cd3ea132d
jeachnuy <jae_0107@naver.com>
2022-08-02 04:15:27 +0900

    Initial commit

---
+++ src/main/java/org/eternity/food/market/infra/generic/money/RatioConverter.java
@@ -0,0 +1,4 @@
+package org.eternity.food.market.infra.generic.money;
+
+public class RatioConverter {
+}
---
+++ src/test/java/org/eternity/food/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eternity.food.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
